90c95e80fa73d02bf2b38f027632b1b4ecd8c973
welkin22 <xwmbxx@gmail.com>
2024-10-11 14:19:17 +0800

    add txlevel metrics

--- core/types/dag.go
+++ core/types/dag.go
@@ -164,17 +164,19 @@ func ValidatePlainTxDAG(d TxDAG, txCnt int) error {
 		if dep == nil {
 			return fmt.Errorf("PlainTxDAG contains nil txdep, tx: %v", i)
 		}
-		for j, tx := range dep.TxIndexes {
-			if tx >= uint64(i) || tx >= uint64(txCnt) {
-				return fmt.Errorf("PlainTxDAG contains the exceed range dependency, tx: %v", i)
-			}
-			if j > 0 && dep.TxIndexes[j] <= dep.TxIndexes[j-1] {
-				return fmt.Errorf("PlainTxDAG contains unordered dependency, tx: %v", i)
-			}
-		}
 		if dep.Flags != nil && *dep.Flags & ^TxDepFlagMask > 0 {
 			return fmt.Errorf("PlainTxDAG contains unknown flags, flags: %v", *dep.Flags)
 		}
+		deps := TxDependency(d, i)
+		for j, tx := range deps {
+			if tx >= uint64(i) || tx >= uint64(txCnt) {
+				return fmt.Errorf("PlainTxDAG contains the exceed range dependency, tx: %v", i)
+			}
+			if j > 0 && deps[j] <= deps[j-1] {
+				return fmt.Errorf("PlainTxDAG contains unordered dependency, tx: %v", i)
+			}
+		}
+
 	}
 	return nil
 }
